210bb95272db229dd99bd52ade4c84e211b4ea56
Lucas Jesus <cc19372@g.unicamp.br>
2021-09-26 22:36:58 -0300

    Falta config a geracao do result e criar CI

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculadora.SOMAR(4D, 5D));
+        System.out.println(Calculadora.SUBTRAIR(4D, 5D));
+        System.out.println(Calculadora.MULTIPLICAR(4D, 5D));
+        System.out.println(Calculadora.DIVIDIR(4D, 5D));
+    }
+}
